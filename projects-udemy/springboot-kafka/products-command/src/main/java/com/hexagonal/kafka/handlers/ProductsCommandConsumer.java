6c3654ff50890b35eb7fc7a7798b2440057bd095
david garcia <manuonda@gmail.com>
2026-06-15 17:06:49 -0300

    Feat(products-command): Integrate ProductService into Kafka consumer for CREATE command

--- projects-udemy/springboot-kafka/products-command/src/main/java/com/hexagonal/kafka/handlers/ProductsCommandConsumer.java
+++ projects-udemy/springboot-kafka/products-command/src/main/java/com/hexagonal/kafka/handlers/ProductsCommandConsumer.java
@@ -3,6 +3,8 @@ package com.hexagonal.kafka.handlers;
 
 import com.hexagonal.kafka.models.Command;
 import com.hexagonal.kafka.models.dto.ProductDto;
+import com.hexagonal.kafka.service.ProductService;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
@@ -19,14 +21,38 @@ public class ProductsCommandConsumer {
 
     private static final Logger log = LoggerFactory.getLogger(ProductsCommandConsumer.class);
 
+    private final ProductService productService;
+    public ProductsCommandConsumer(ProductService productService) {
+        this.productService = productService;
+    }
+
     // Bean que define un consumidor de comandos para productos.
     // Este consumidor recibe comandos de tipo Command<ProductDto>
     @Bean
     public Consumer<Command<ProductDto>>  handleCommands(){
      return command ->{
           log.info("Received command con type={}, id={}, body={}", command.type(), command.id(), command.body());
-           // Aquí se implementaría la lógica para manejar el comando, por ejemplo, llamar a un
-     };
+          String type =  command.type();
+          switch(type) {
+            case "CREATE" -> {
+                if (command.body() == null) {
+                    log.warn("Create command body is null");
+                    return;
+                }
+                log.info("Creating product with name={}, price={}", command.body().name(), command.body().price());
+                ProductDto productDto = command.body();
+                this.productService.create(productDto);
+                log.info("Product created with id={}", productDto.id());
+                return;
+            }
+            case "UPDATE" -> {}
+            case "DELETE" -> {}
+            case "GET" -> {}
+            default -> {}
+
+          }
+
+        };
     }
 
 }
