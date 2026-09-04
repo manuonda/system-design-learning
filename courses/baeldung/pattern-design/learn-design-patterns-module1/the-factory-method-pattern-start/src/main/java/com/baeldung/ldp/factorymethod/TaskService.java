a30979cb31d511b91504bb590b9165eb4afab697
david garcia <manuonda@gmail.com>
2026-09-04 07:09:53 -0300

    refactor: Update TaskService to use Factory Method

--- courses/baeldung/pattern-design/learn-design-patterns-module1/the-factory-method-pattern-start/src/main/java/com/baeldung/ldp/factorymethod/TaskService.java
+++ courses/baeldung/pattern-design/learn-design-patterns-module1/the-factory-method-pattern-start/src/main/java/com/baeldung/ldp/factorymethod/TaskService.java
@@ -2,12 +2,10 @@ package com.baeldung.ldp.factorymethod;
 
 public class TaskService {
 
-    public Task createTask(String type, String name) {
-        if ("pdf".equals(type)) {
-            return new PdfExportTask(name);
-        } else if ("csv".equals(type)) {
-            return new CsvExportTask(name);
-        }
-        throw new IllegalArgumentException("Unknown task type: " + type);
+    private final TaskCreator taskCreator;
+
+    public TaskService(TaskCreator taskCreator) {
+        this.taskCreator = taskCreator;
     }
+
 }
